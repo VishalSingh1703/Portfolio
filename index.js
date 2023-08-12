c2bd5742eafaf0303340bfcb659b4324de81726a
VishalSingh1703 <113925017+VishalSingh1703@users.noreply.github.com>
2023-08-12 23:37:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Smooth scrolling to move viewport down
+// document.addEventListener("scroll", function () {
+//     const cards = document.querySelectorAll(".card");
+//     cards.forEach((card) => {
+//       const rect = card.getBoundingClientRect();
+//       if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+//         card.style.opacity = "1";
+//         card.style.transform = "translateX(0)";
+//       }
+//     });
+//   });
+  
